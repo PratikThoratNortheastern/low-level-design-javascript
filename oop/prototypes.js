ad5f0f8606f583488f155d6e879ae9e84a178997
Pratik Thorat <thorat.p@northeastern.edu>
2026-03-25 17:37:19 -0400

    Older way to use prototypes added

--- oop/prototypes.js
+++ oop/prototypes.js
@@ -15,3 +15,29 @@ let tesla = { driver: 'AI' };
 Object.setPrototypeOf(tesla, genericCar); // same as tesla = {driver: 'AI', __proto__: genericCar}
 
 console.log(Object.getPrototypeOf(tesla));
+
+// Another way to sort of add prototypes
+
+const functionBundle = {
+  addMoney: function () {
+    this.accountBalance++;
+  },
+  fetchBalance: function () {
+    console.log(`The balance is ${this.accountBalance}`);
+  },
+};
+
+function createCustomer(name, accountBalance, branch) {
+  const customer = Object.create(functionBundle);
+  customer.name = name;
+  customer.accountBalance = accountBalance;
+  customer.branch = branch;
+
+  return customer;
+}
+
+const customer1 = createCustomer('Allan', 10_000_000_000, 'Malad West');
+customer1.addMoney();
+customer1.addMoney();
+customer1.fetchBalance();
+console.table(customer1);
